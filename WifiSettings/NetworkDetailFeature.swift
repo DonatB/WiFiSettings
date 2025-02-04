8faccc59ac36f8d633791d3a05ad8330b3e44952
Donat Bajrami <donatbajrami@hotmail.com>
2025-02-04 13:12:15 +0100

    WifiSettings refactored a bit

--- WifiSettings/NetworkDetailFeature.swift
+++ WifiSettings/NetworkDetailFeature.swift
@@ -1,11 +1,3 @@
-//
-//  NetworkDetailFeature.swift
-//  WifiSettings
-//
-//  Created by Ptera on 1/20/25.
-//
-
-import Foundation
 import Observation
 import UIKit
 import UIKitNavigation
@@ -13,97 +5,90 @@ import UIKitNavigation
 @MainActor
 @Perceptible
 class NetworkDetailModel {
-    var forgetAlertIsPresented = false
-    let onConfirmForget: () -> Void
-    let network: Network
-    
-    init(network: Network, onConfirmForget: @escaping () -> Void) {
-        self.network = network
-        self.onConfirmForget = onConfirmForget
-    }
-    
-    func forgetNetworkButtonTapped() {
-        forgetAlertIsPresented = true
-    }
-    
-    func confirmForgetNetworkButtonTapped() {
-        onConfirmForget()
-    }
+  var forgetAlertIsPresented = false
+  let onConfirmForget: () -> Void
+  let network: Network
+
+  init(
+    forgetAlertIsPresented: Bool = false,
+    network: Network,
+    onConfirmForget: @escaping () -> Void = {}
+  ) {
+    self.forgetAlertIsPresented = forgetAlertIsPresented
+    self.onConfirmForget = onConfirmForget
+    self.network = network
+  }
+
+  func forgetNetworkButtonTapped() {
+    forgetAlertIsPresented = true
+  }
+
+  func confirmForgetNetworkButtonTapped() {
+    onConfirmForget()
+  }
 }
 
-final class NetworkDetailsViewController: UIViewController {
-    @UIBindable var model: NetworkDetailModel
-    
-    init(model: NetworkDetailModel) {
-        self.model = model
-        super.init(nibName: nil, bundle: nil)
+final class NetworkDetailViewController: UIViewController {
+  @UIBindable var model: NetworkDetailModel
+
+  init(model: NetworkDetailModel) {
+    self.model = model
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemBackground
+    navigationItem.title = model.network.name
+
+    let forgetButton = UIButton(type: .system, primaryAction: UIAction { [weak self] _ in
+      guard let self else { return }
+      model.forgetNetworkButtonTapped()
+    })
+    forgetButton.setTitle("Forget network", for: .normal)
+    forgetButton.setTitleColor(.red, for: .normal)
+    forgetButton.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(forgetButton)
+    NSLayoutConstraint.activate([
+      forgetButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      forgetButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+    ])
+
+    // present(<#T##viewControllerToPresent: UIViewController##UIViewController#>, animated: <#T##Bool#>)
+
+    present(isPresented: $model.forgetAlertIsPresented) { [model] in
+      let controller = UIAlertController(
+        title: "Forget Wi-Fi Network “\(model.network.name)”?",
+        message: """
+          Your devices using iCloud Keychain will no longer join this Wi-Fi \
+          network.
+          """,
+        preferredStyle: .alert
+      )
+      controller.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { _ in }))
+      controller.addAction(UIAlertAction(title: "Forget", style: .destructive, handler: { _ in
+        model.confirmForgetNetworkButtonTapped()
+      }))
+      return controller
     }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        navigationItem.title = model.network.name
-        
-        let forgetButton = UIButton(type: .system, primaryAction: UIAction { [weak self] _ in
-            guard let self else { return }
-            print("Tapperooski")
-            model.forgetNetworkButtonTapped()
-        })
-        forgetButton.setTitle("Forget network", for: .normal)
-        forgetButton.setTitleColor(.systemMint, for: .normal)
-        forgetButton.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(forgetButton)
-        NSLayoutConstraint.activate([
-            forgetButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            forgetButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
-        
-        present(
-            isPresented: $model.forgetAlertIsPresented,
-            content: { [model] in
-                let controller = UIAlertController(
-                    title: "Forget Wi-Fi Network \(model.network.name)",
-                    message: """
-                    Your iPhone and other devices using iCloud keychain will no longer join the Wi-Fi network
-                    """,
-                    preferredStyle: .alert
-                )
-                controller.addAction(
-                    UIAlertAction(
-                        title: "Cancel",
-                        style: .cancel
-                    )
-                )
-                controller
-                    .addAction(
-                        UIAlertAction(
-                            title: "Forget",
-                            style: .destructive,
-                            handler: { _ in
-                                model.confirmForgetNetworkButtonTapped()
-                            }
-                        )
-                    )
-                return controller
-            }
-        )
-    }
+  }
 }
 
 import SwiftUI
-#Preview{
-    UIViewControllerRepresenting {
-        UINavigationController(
-            rootViewController: NetworkDetailsViewController(
-                model: NetworkDetailModel(
-                    network: Network(name: "Blob's WiFi"),
-                    onConfirmForget: {}
-                )
-            )
+#Preview {
+  UIViewControllerRepresenting {
+    UINavigationController(
+      rootViewController: NetworkDetailViewController(
+        model: NetworkDetailModel(
+          forgetAlertIsPresented: true,
+          network: Network(name: "Blob's WiFi"),
+          onConfirmForget: { }
         )
-    }
+      )
+    )
+  }
 }
--- WifiSettings/WiFiSettingsFeature.swift
+++ WifiSettings/WiFiSettingsFeature.swift
@@ -1,11 +1,3 @@
-//
-//  WiFiSettingsFeature.swift
-//  WiFiSettings
-//
-//  Created by Ptera on 1/20/25.
-//
-
-import Foundation
 import Observation
 import UIKit
 import UIKitNavigation
@@ -13,225 +5,225 @@ import UIKitNavigation
 @Perceptible
 @MainActor
 class WiFiSettingsModel {
-    @CasePathable
-    enum Destination {
-        case connect(ConnectToNetworkModel)
-        case detail(NetworkDetailModel)
-    }
-    
-    var destination: Destination?
-    var foundNetworks: [Network]
-    var isOn: Bool
-    var selectedNetworkID: Network.ID?
-    
-    init(foundNetworks: [Network], isOn: Bool = true, selectedNetworkID: Network.ID? = nil) {
-        self.foundNetworks = foundNetworks
-        self.isOn = isOn
-        self.selectedNetworkID = selectedNetworkID
-    }
-    
-    func networkTapped(_ network: Network) {
-        if network.id == selectedNetworkID {
-            self.destination = .detail(
-                NetworkDetailModel(
-                    network: network,
-                    onConfirmForget: { [weak self] in
-                        guard let self else { return }
-                        destination = nil
-                        selectedNetworkID = nil
-                    }
-                )
-            )
-        } else if network.isSecured {
-            destination = .connect(
-                ConnectToNetworkModel(
-                    network: network,
-                    onConnect: { [weak self] network in
-                        guard let self else { return }
-                        destination = nil
-                        selectedNetworkID = network.id
-                    }
-                )
-            )
-        } else {
-            selectedNetworkID = network.id
-        }
-    }
-    
-    func infoButtonTapped(network: Network) {
-        self.destination = .detail(
-            NetworkDetailModel(
-                network: network,
-                onConfirmForget: { [weak self] in
-                    guard let self else { return }
-                    destination = nil
-                    selectedNetworkID = nil
-                }
-            )
+  @CasePathable
+  enum Destination {
+    case connect(ConnectToNetworkModel)
+    case detail(NetworkDetailModel)
+  }
+
+  var destination: Destination?
+  var foundNetworks: [Network]
+  var isOn: Bool
+  var selectedNetworkID: Network.ID?
+
+  init(
+    destination: Destination? = nil,
+    foundNetworks: [Network],
+    isOn: Bool = true,
+    selectedNetworkID: Network.ID? = nil
+  ) {
+    self.destination = destination
+    self.foundNetworks = foundNetworks
+    self.isOn = isOn
+    self.selectedNetworkID = selectedNetworkID
+  }
+
+  func networkTapped(_ network: Network) {
+    if network.id == selectedNetworkID {
+      self.destination = .detail(
+        NetworkDetailModel(
+          network: network,
+          onConfirmForget: { [weak self] in
+            guard let self else { return }
+            destination = nil
+            selectedNetworkID = nil
+          }
         )
+      )
+    } else if network.isSecured {
+      destination = .connect(
+        ConnectToNetworkModel(
+          network: network,
+          onConnect: { [weak self] network in
+            guard let self else { return }
+            destination = nil
+            selectedNetworkID = network.id
+          }
+        )
+      )
+    } else {
+      selectedNetworkID = network.id
     }
+  }
+
+  func infoButtonTapped(network: Network) {
+    destination = .detail(
+      NetworkDetailModel(
+        network: network,
+        onConfirmForget: { [weak self] in
+          guard let self else { return }
+          destination = nil
+          selectedNetworkID = nil
+        }
+      )
+    )
+  }
 }
 
 class WiFiSettingsViewController: UICollectionViewController {
-    @UIBindable var model: WiFiSettingsModel
-    var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
-    
-    init(model: WiFiSettingsModel) {
-        self.model = model
-        super.init(
-            collectionViewLayout:
-                UICollectionViewCompositionalLayout.list(
-                    using: UICollectionLayoutListConfiguration(
-                        appearance: .insetGrouped
-                    )
-                )
+  @UIBindable var model: WiFiSettingsModel
+  var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
+
+  init(model: WiFiSettingsModel) {
+    self.model = model
+    super.init(
+      collectionViewLayout: UICollectionViewCompositionalLayout.list(
+        using: UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+      )
+    )
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    navigationItem.title = "Wi-Fi"
+
+    let cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, Item> {
+      [weak self] cell, indexPath, item in
+      guard let self else { return }
+      configure(cell: cell, indexPath: indexPath, item: item)
+    }
+
+    dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView) {
+      collectionView, indexPath, item in
+      collectionView.dequeueConfiguredReusableCell(
+        using: cellRegistration,
+        for: indexPath,
+        item: item
+      )
+    }
+
+    observe { [weak self] in
+      guard let self else { return }
+      dataSource.apply(.init(model: model), animatingDifferences: true)
+    }
+
+    present(item: $model.destination.connect) { model in
+      UINavigationController(
+        rootViewController: ConnectToNetworkViewController(
+          model: model
         )
+      )
     }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+
+      self.navigationDestination(item: $model.destination.detail) { model in
+      NetworkDetailViewController(model: model)
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        navigationItem.title = "Wi-Fi"
-        
-        let cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, Item> { [weak self] cell, indexPath, item in
-            guard let self else { return }
-            configure(cell: cell, indexPath: indexPath, item: item)
-        }
-        
-        dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView) { collectionView, indexPath, item in
-            collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: item)
-        }
-        
-        observe { [weak self] in
-          guard let self else { return }
-          dataSource.apply(
-            .init(model: model),
-            animatingDifferences: true
+  }
+
+  override func collectionView(
+    _ collectionView: UICollectionView,
+    didSelectItemAt indexPath: IndexPath
+  ) {
+    guard case let .foundNetwork(network) = dataSource.itemIdentifier(for: indexPath)
+    else { return }
+    model.networkTapped(network)
+  }
+
+  private func configure(
+    cell: UICollectionViewListCell,
+    indexPath: IndexPath,
+    item: Item
+  ) {
+    var configuration = cell.defaultContentConfiguration()
+    defer { cell.contentConfiguration = configuration }
+
+    switch item {
+    case .isOn:
+      configuration.text = "Wi-Fi"
+      cell.accessories = [
+        .customView(
+          configuration: UICellAccessory.CustomViewConfiguration(
+            customView: UISwitch(isOn: $model.isOn),
+            placement: .trailing(displayed: .always)
           )
-        }
-        
-        present(item: $model.destination.connect) { model in
-            UINavigationController(
-                rootViewController: ConnectToNetworkViewController(model: model)
-            )
-        }
-        
-        navigationDestination(
-          item: $model.destination.detail
-        ) { model in
-            NetworkDetailsViewController(model: model)
-        }
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        guard case let .foundNetwork(network) = dataSource.itemIdentifier(for: indexPath) else { return }
-        model.networkTapped(network)
-    }
-    
-    private func configure(
-        cell: UICollectionViewListCell,
-        indexPath: IndexPath,
-        item: Item
-    ) {
-        var configuration = cell.defaultContentConfiguration()
-        defer { cell.contentConfiguration = configuration }
-        
-        switch item {
-            case .isOn:
-                configuration.text = "Wi-Fi"
-                cell.accessories = [
-                    .customView(
-                        configuration: UICellAccessory.CustomViewConfiguration(
-                            customView: UISwitch(isOn: $model.isOn),
-                            placement: .trailing(displayed: .always)
-                        )
-                    )
-                ]
-            case let .selectedNetwork(networkID):
-                guard let network = model.foundNetworks.first(where: { $0.id == networkID }) else { return }
-                configureNetwork(
-                    configuration: &configuration,
-                    cell: cell,
-                    network: network,
-                    indexPath: indexPath,
-                    item: item
-                )
-            case let .foundNetwork(network):
-                configureNetwork(
-                    configuration: &configuration,
-                    cell: cell,
-                    network: network,
-                    indexPath: indexPath,
-                    item: item
-                )
-        }
-    }
-    
-    func configureNetwork(
-        configuration: inout UIListContentConfiguration,
-        cell: UICollectionViewListCell,
-        network: Network,
-        indexPath: IndexPath,
-        item: Item
-    ) {
-        configuration.text = network.name
-        cell.accessories = [
-            .detail(displayed: .always) { [weak self] in
-                guard let self else { return }
-                model.infoButtonTapped(network: network)
-            }
-        ]
-        if network.isSecured {
-            let image = UIImage(systemName: "lock.fill")!
-            let imageView = UIImageView(image: image)
-            imageView.tintColor = .darkText
-            cell.accessories.append(
-                .customView(
-                    configuration:
-                        UICellAccessory.CustomViewConfiguration(
-                            customView: imageView,
-                            placement: .trailing(displayed: .always)
-                        )
-                )
-            )
-        }
-        let image = UIImage(
-            systemName: "wifi", variableValue: network.connectivity
-        )!
-        let imageView = UIImageView(image: image)
-        imageView.tintColor = .darkText
-        cell.accessories.append(
-            .customView(
-                configuration:
-                    UICellAccessory.CustomViewConfiguration(
-                        customView: imageView,
-                        placement: .trailing(displayed: .always)
-                    )
-            )
         )
-        if network.id == model.selectedNetworkID {
-            cell.accessories.append(
-                .customView(
-                    configuration:
-                        UICellAccessory.CustomViewConfiguration(
-                            customView: UIImageView(
-                                image: UIImage(systemName: "checkmark")!
-                            ),
-                            placement: .leading(displayed: .always),
-                            reservedLayoutWidth: .custom(1)
-                        )
-                )
-            )
-        }
+      ]
+    case let .selectedNetwork(networkID):
+      guard let network = model.foundNetworks.first(where: { $0.id == networkID })
+      else { return }
+      configureNetwork(
+        configuration: &configuration,
+        cell: cell,
+        network: network,
+        indexPath: indexPath,
+        item: item
+      )
+    case let .foundNetwork(network):
+      configureNetwork(
+        configuration: &configuration,
+        cell: cell,
+        network: network,
+        indexPath: indexPath,
+        item: item
+      )
     }
+  }
 
-    
-}
+  func configureNetwork(
+    configuration: inout UIListContentConfiguration,
+    cell: UICollectionViewListCell,
+    network: Network,
+    indexPath: IndexPath,
+    item: Item
+  ) {
+    configuration.text = network.name
+    cell.accessories = [
+      .detail(displayed: .always) { [weak self] in
+        guard let self else { return }
+        model.infoButtonTapped(network: network)
+      }
+    ]
+    if network.isSecured {
+      let image = UIImage(systemName: "lock.fill")!
+      let imageView = UIImageView(image: image)
+      imageView.tintColor = .darkText
+      cell.accessories.append(
+        .customView(
+          configuration: UICellAccessory.CustomViewConfiguration(
+            customView: imageView,
+            placement: .trailing(displayed: .always)
+          )
+        )
+      )
+    }
+    let image = UIImage(systemName: "wifi", variableValue: network.connectivity)!
+    let imageView = UIImageView(image: image)
+    imageView.tintColor = .darkText
+    cell.accessories.append(
+      .customView(
+        configuration: UICellAccessory.CustomViewConfiguration(
+          customView: imageView,
+          placement: .trailing(displayed: .always)
+        )
+      )
+    )
+    if network.id == model.selectedNetworkID {
+      cell.accessories.append(
+        .customView(
+          configuration: UICellAccessory.CustomViewConfiguration(
+            customView: UIImageView(image: UIImage(systemName: "checkmark")!),
+            placement: .leading(displayed: .always),
+            reservedLayoutWidth: .custom(1)
+          )
+        )
+      )
+    }
+  }
 
-extension WiFiSettingsViewController {
   enum Section {
     case top
     case foundNetworks
@@ -245,73 +237,60 @@ extension WiFiSettingsViewController {
 }
 
 extension NSDiffableDataSourceSnapshot<WiFiSettingsViewController.Section, WiFiSettingsViewController.Item> {
-    @MainActor
-    init(model: WiFiSettingsModel) {
-        self.init()
-        
-        appendSections([.top])
-        appendItems([.isOn], toSection: .top)
-        
-        guard model.isOn else { return }
-        
-        if let selectedNetworkID = model.selectedNetworkID {
-          appendItems(
-            [.selectedNetwork(selectedNetworkID)],
-            toSection: .top
-          )
-        }
+  @MainActor
+  init(model: WiFiSettingsModel) {
+    self.init()
 
-        
-        appendSections([.foundNetworks])
-        
-        appendItems(
-          model.foundNetworks
-            .filter { $0.id != model.selectedNetworkID }
-            .map { .foundNetwork($0) },
-          toSection: .foundNetworks
-        )
+    appendSections([.top])
+    appendItems([.isOn], toSection: .top)
 
+    guard model.isOn
+    else { return }
+
+    if let selectedNetworkID = model.selectedNetworkID {
+      appendItems([.selectedNetwork(selectedNetworkID)], toSection: .top)
     }
+
+    appendSections([.foundNetworks])
+
+    appendItems(
+      model.foundNetworks
+        .filter { $0.id != model.selectedNetworkID }
+        .map { .foundNetwork($0) },
+      toSection: .foundNetworks
+    )
+  }
 }
 
 import SwiftUI
-
 #Preview {
-    let model = WiFiSettingsModel(foundNetworks: .mocks)
-    UIViewControllerRepresenting {
-        UINavigationController(
-            rootViewController: WiFiSettingsViewController(model: model)
-        )
-    }
-    .task {
-        while true {
-            try? await Task.sleep(for: .seconds(1))
-            guard Bool.random() else { continue }
-            if Bool.random() {
-                guard
-                    let randomIndex = (
-                        0..<model.foundNetworks.count
-                    )
-                        .randomElement()
-                else { continue }
-                if model.foundNetworks[randomIndex].id
-                    != model.selectedNetworkID
-                {
-                    model.foundNetworks.remove(at: randomIndex)
-                }
-            } else {
-                model.foundNetworks.append(
-                    Network(
-                        name: goofyWiFiNames.randomElement()!,
-                        isSecured: !(1...1_000).randomElement()!
-                            .isMultiple(of: 5),
-                        connectivity: Double((1...100).randomElement()!)
-                        / 100
-                    )
-                )
-            }
+  let model = WiFiSettingsModel(foundNetworks: .mocks)
+  return UIViewControllerRepresenting {
+    UINavigationController(
+      rootViewController: WiFiSettingsViewController(model: model)
+    )
+  }
+  .task {
+    while true {
+      try? await Task.sleep(for: .seconds(1))
+      guard Bool.random() else { continue }
+      if Bool.random() && Bool.random() {
+        guard let randomIndex = (0..<model.foundNetworks.count).randomElement()
+        else { continue }
+        if model.foundNetworks[randomIndex].id != model.selectedNetworkID {
+          model.foundNetworks.remove(at: randomIndex)
         }
+      } else {
+        model.foundNetworks.append(
+          Network(
+            name: goofyWiFiNames.randomElement()!,
+            isSecured: !(1...1_000).randomElement()!.isMultiple(of: 5),
+            connectivity: Double((1...100).randomElement()!) / 100
+          )
+        )
+      }
     }
+  }
 }
 
 let goofyWiFiNames = [
@@ -333,7 +312,6 @@ let goofyWiFiNames = [
   "TheRoomWiFi" ,
   "PrivateSpace",
 ]
-
 
 extension [Network] {
   static let mocks = [
@@ -359,6 +337,3 @@ extension [Network] {
     ),
   ]
 }
-
-
-
